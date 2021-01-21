6065ea0a6ca21ce75fc634740f2a74ac559ca779
masummim50 <33899137+masummim50@users.noreply.github.com>
2021-01-21 23:34:58 +0600

    all done

---
+++ assignment.js
@@ -0,0 +1,59 @@
+
+
+// First problem
+function kilometerToMeter(km){
+  if (km < 0){
+    console.log("distance can't be a negetive value");
+  }
+  else{
+    return km*1000;
+  }
+};
+
+// second problem
+function budgetCalculator(watch, mobile, laptop){
+  if(watch<0 || mobile<0 || laptop<0){
+    return "can't buy negetive amount of excessories, try 0 amount instead";
+  }
+  else{
+    var watchPrice = watch * 50;
+    var mobilePrice = mobile * 100;
+    var laptopPrice = laptop * 500;
+  }
+  var totalPrice = watchPrice + mobilePrice + laptopPrice;
+  return totalPrice;
+};
+
+// third problem
+function hotelCost(days){
+  if(days < 0){
+    return "stay in a hotel first"
+  }
+  else if(days<=10){
+    var totalCost = days * 100;
+  }
+  else if(days<=20){
+    var daysWithDiscount = days - 10;
+    var discountCost = daysWithDiscount * 80;
+    var totalCost = 1000+ discountCost;
+  }
+  else{
+    var daysWithDiscount = days - 20;
+    var discountCost = daysWithDiscount * 50;
+    var totalCost = 1800 + discountCost;
+  }
+  return totalCost;
+};
+
+// fourth problem
+function megaFriend(friends){
+  var largestLength = 0;
+  for (var i = 0; i<friends.length; i++){
+    var nameLength = friends[i].length;
+    if(nameLength>largestLength){
+      largestLength = nameLength;
+      var longerName = friends[i];
+    }
+  }
+  return longerName;
+}
